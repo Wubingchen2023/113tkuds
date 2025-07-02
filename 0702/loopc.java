fc5151b15b19030e053dbd082ae2b987295838bf
Wubingchen2023 <0901370213peter@gmail.com>
2025-07-02 10:46:47 +0800

    練習
    add loopa.java

---
+++ 0702/loopc.java
@@ -0,0 +1,12 @@
+
+public class loopc {
+    public static void main(String[] args) {
+        int x = -100;
+        int count = 0;
+        do { 
+            x=x+1;
+            count++;
+        } while (x++ <=100);
+        System.out.println(count);
+    }
+}
